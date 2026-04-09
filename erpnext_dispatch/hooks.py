d41942876ce52811faadbeab4b6dc083b2540a86
Mseethaler <michaels@nc.rr.com>
2026-04-09 17:35:47 -0400

    fixed stuff

--- erpnext_dispatch/hooks.py
+++ erpnext_dispatch/hooks.py
@@ -1,25 +1,35 @@
 app_name = "erpnext_dispatch"
 app_title = "ERPNext Dispatch"
-app_publisher = "Digital Soveriengty"
+app_publisher = "Digital Sovereignty"
 app_description = "Field service dispatch workflow for ERPNext"
 app_version = "0.0.1"
 app_email = "info@digital-sovereignty.cc"
 app_license = "MIT"
 
+after_install = "erpnext_dispatch.setup.install.after_install"
+
 # Fixtures to export/import with the app
 fixtures = [
-    # Custom fields on core doctypes
+    # Scheduled date/time fields on Opportunity
     {
         "dt": "Custom Field",
         "filters": [
             ["dt", "in", ["Opportunity", "Sales Order", "Sales Invoice", "Quotation"]],
-            ["module", "=", "ERPNext Dispatch"]
+            ["fieldname", "like", "custom_scheduled%"],
         ]
     },
-    # Workspace
+    # Signature fields
+    {
+        "dt": "Custom Field",
+        "filters": [
+            ["dt", "in", ["Quotation", "Sales Order", "Sales Invoice"]],
+            ["fieldname", "like", "custom_%signature%"],
+        ]
+    },
+    # Workspaces
     {
         "dt": "Workspace",
-        "filters": [["name", "=", "Dispatch Tecs"]]
+        "filters": [["name", "in", ["Dispatched Tecs", "Dispatch Officer", "Quick Routes"]]]
     },
     # Calendar View
     {
@@ -29,15 +39,12 @@ fixtures = [
     # Notification configs
     {
         "dt": "Notification",
-        "filters": [["module", "=", "ERPNext Dispatch"]]
+        "filters": [["name", "like", "Dispatch%"]]
     },
 ]
 
 # JS includes per doctype
 doctype_js = {
-    "Opportunity"   : "public/js/dispatch.js",
-    "Sales Order"   : "public/js/dispatch.js",
+    "Opportunity": "public/js/dispatch.js",
+    "Sales Order": "public/js/dispatch.js",
 }
-
-# Webhooks or doc events can go here later
-# doc_events = {}
